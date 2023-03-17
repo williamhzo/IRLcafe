908296dc231f82076ca76859b2347c9d5f73838e
Basile Nouvellet <b.nouvellet@gmail.com>
2023-03-17 01:52:05 +0000

    Fix Mapbox issues

--- assets/svelte/Mapbox.svelte
+++ assets/svelte/Mapbox.svelte
@@ -1,17 +1,17 @@
 <script>
-  import { onMount } from 'svelte';
-  import mapboxgl from 'mapbox-gl';
-  import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+  import { onMount } from "svelte";
+  import mapboxgl from "mapbox-gl";
+  import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
   const MAPBOX_ACCESS_TOKEN =
-    'pk.eyJ1Ijoid2lsbGlhbS1oem8iLCJhIjoiY2xmYnByZHEwMGZxMzNvbXZmdDB3bTlyZyJ9.nz4hALRlNGSKV8N-nySK2g';
+    "pk.eyJ1Ijoid2lsbGlhbS1oem8iLCJhIjoiY2xmYnByZHEwMGZxMzNvbXZmdDB3bTlyZyJ9.nz4hALRlNGSKV8N-nySK2g";
 
   onMount(() => {
     mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 
     const map = new mapboxgl.Map({
-      container: 'map-container',
-      style: 'mapbox://styles/mapbox/streets-v12',
+      container: "map-container",
+      style: "mapbox://styles/mapbox/streets-v12",
       center: [12.65147, 55.608166],
       zoom: 12,
     });
@@ -20,8 +20,6 @@
     const marker = new mapboxgl.Marker()
       .setLngLat([-122.4194, 37.7749])
       .addTo(map);
-
-    map.addMarker(marker);
 
     // Add a geocoder control to the map
     const geocoder = new MapboxGeocoder({
@@ -32,5 +30,10 @@
     map.addControl(geocoder);
   });
 </script>
+
+<link
+  href="https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.css"
+  rel="stylesheet"
+/>
 
 <div id="map-container" class="w-full h-screen" />

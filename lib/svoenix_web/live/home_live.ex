4117f561eebff3fc8a5f2a149cea42df795cb642
William H <william.hermozo@protonmail.com>
2023-03-17 20:32:22 +0000

    add home base content

--- lib/svoenix_web/live/home_live.ex
+++ lib/svoenix_web/live/home_live.ex
@@ -5,16 +5,32 @@ defmodule SvoenixWeb.HomeLive do
 
   def render(assigns) do
     ~H"""
-    <Svelte.render id="example-svelte" name="Example" />
+    <section class="flex flex-col items-center gap-12 px-4 py-12 container">
+      <header class="flex flex-col gap-1 items-center">
+        <h1 class="tracking widest text-3xl font-semibold">coffree</h1>
+        <h2 class="text-lg text-slate-500">find the best spots near you</h2>
+      </header>
+
+      <form for={@form} phx-submit="update_city">
+        <label for={@form}>
+          <input
+            field={@form[:city]}
+            type="text"
+            placeholder="Lisbon, Portugal"
+            label="City"
+            autocomplete="new-password"
+          />
+        </label>
+
+        <button type="submit">find some spots</button>
+      </form>
+    </section>
     """
   end
 
-  def handle_event("set_number", %{"number" => number}, socket) do
-    {:noreply, assign(socket, :number, number)}
-  end
-
   def mount(_params, _session, socket) do
-    socket = assign(socket, :number, 0)
+    socket = assign(socket, city_form: %{}, toForm(:city_form))
+
     {:ok, socket}
   end
 end

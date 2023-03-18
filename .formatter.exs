fc1e079c5764d5333f0c7389fba0eadbaa94a4d1
Basile Nouvellet <b.nouvellet@gmail.com>
2023-03-18 12:28:31 +0000

    Format .formatter file

--- .formatter.exs
+++ .formatter.exs
@@ -2,6 +2,11 @@
   import_deps: [:ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
   # plugins: [Phoenix.LiveView.HTMLFormatter],
+  # plugins: [Phoenix.LiveView.HTMLFormatter, TailwindFormatter],
   plugins: [TailwindFormatter.MultiFormatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
+  inputs: [
+    "*.{heex,ex,exs}",
+    "{config,lib,test}/**/*.{heex,ex,exs}",
+    "priv/*/seeds.exs"
+  ]
 ]

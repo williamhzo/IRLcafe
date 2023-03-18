5a8f4863ef5c65d5697e5288792fbdc22ccbf0f5
William H <william.hermozo@protonmail.com>
2023-03-18 20:55:09 +0000

    style tweaks + formatting

--- assets/js/hooks/svelteHook.js
+++ assets/js/hooks/svelteHook.js
@@ -1,14 +1,14 @@
-import Example from "../components/Example.svelte";
+import Slots from '../components/Slots.svelte';
 
 const components = {
-  Example,
+  Slots,
 };
 
 export const SvelteHook = {
   mounted() {
     const componentName = this.el.dataset.name;
     if (!componentName)
-      throw new Error("Svelte Component name must be provided");
+      throw new Error('Svelte Component name must be provided');
 
     const component = components[componentName];
     if (!component)
@@ -41,6 +41,6 @@ export const SvelteHook = {
     this.pushEvent(event, data, callback);
   },
   goto(href) {
-    liveSocket.pushHistoryPatch(href, "push", this.el);
+    liveSocket.pushHistoryPatch(href, 'push', this.el);
   },
 };
--- lib/svoenix_web/components/header.ex
+++ lib/svoenix_web/components/header.ex
@@ -7,16 +7,16 @@ defmodule SvoenixWeb.Header do
 
   def render(assigns) do
     ~H"""
-    <header class="h-[52px] bg-opacity-[0.75] py-2 sticky top-0 z-20 flex items-center border-b border-gray-100 bg-white px-4 backdrop-blur-lg">
+    <header class="bg-opacity-[0.75] py-2 sticky top-0 z-20 flex items-center border-b border-gray-100 bg-white px-4 backdrop-blur-lg">
       <div class="w-main-content mx-auto flex items-center justify-between">
-        <.link class="text-lg font-bold hover:text-gray-700" navigate={~p"/"}>
-          IRLcafé
+        <.link class="text-lg font-bold hover:text-brand-text-hover" navigate={~p"/"}>
+          IRL café 👨‍💻☕
         </.link>
 
         <div :if={@city} class="flex flex-col items-end">
           <small class="text-muted">city</small>
 
-          <.link navigate={"/#{@city.slug}"} class="font-semibold hover:text-gray-700">
+          <.link navigate={"/#{@city.slug}"} class="font-semibold hover:text-brand-text-hover">
             <%= @city.name %>
           </.link>
         </div>

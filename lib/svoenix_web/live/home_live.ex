3cfda77ad659785c1916c15b1dfc6d18ddc96c1a
Basile Nouvellet <b.nouvellet@gmail.com>
2023-03-18 23:54:00 +0000

    Rework home page

--- lib/svoenix_web/live/home_live.ex
+++ lib/svoenix_web/live/home_live.ex
@@ -8,19 +8,41 @@ defmodule SvoenixWeb.HomeLive do
     ~H"""
     <section class="container flex flex-col items-center gap-12 px-4 py-12">
       <header class="flex flex-col items-center gap-1">
-        <h1 class="tracking widest text-4xl font-bold">IRLcafé</h1>
-        <h2 class="text-center text-lg text-slate-500">
+        <p class="text-4xl">👨‍💻☕️</p>
+        <h1 class="tracking widest text-4xl font-bold">IRL café</h1>
+        <h2 class="mt-2 text-center text-slate-500">
           find the best spots near you & <br /> meet coffee buddies
         </h2>
       </header>
 
-      <.simple_form for={@form} id="city-form" phx-submit="save">
-        <.input field={@form[:name]} type="text" label="Name" placeholder="Lisbon" />
+      <div class="flex items-center space-x-4">
+        <.form id="city-form" for={@form} class="relative" phx-submit="save">
+          <input
+            id="city-name-input"
+            name="name"
+            type="text"
+            placeholder="lisbon"
+            class={[
+              "w-[12ch] py-4 px-5 text-5xl placeholder:text-gray-400 hover:bg-gray-100 rounded-md",
+              "border-none ring-none outline-none",
+              "focus:border-transparent focus:ring-transparent focus:shadow-none"
+            ]}
+            autocomplete="off"
+            required
+          />
+        </.form>
 
-        <:actions>
-          <.button class="w-full" phx-disable-with="exploring...">explore</.button>
-        </:actions>
-      </.simple_form>
+        <button
+          class="flex items-baseline bg-brand text-white px-4 py-2.5 rounded shadow-lg group"
+          phx-disable-with="exploring..."
+        >
+          <span class="text-lg font-semibold">explore</span>
+          <.icon
+            name="hero-arrow-right-mini"
+            class="h-4 w-4 translate-y-0.5 ml-3 group-hover:translate-x-1 transition-transform"
+          />
+        </button>
+      </div>
     </section>
     """
   end
@@ -33,7 +55,7 @@ defmodule SvoenixWeb.HomeLive do
   end
 
   def handle_event("save", %{"name" => name}, socket) do
-    case name |> String.capitalize() |> get_city() do
+    case name |> String.trim() |> String.capitalize() |> get_city() do
       nil -> {:noreply, socket}
       %City{slug: slug} -> {:noreply, push_navigate(socket, to: ~p"/#{slug}")}
     end

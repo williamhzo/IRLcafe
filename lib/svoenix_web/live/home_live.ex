a9af1dffe8b68b3549b834af7009bc86f576c626
Basile Nouvellet <b.nouvellet@gmail.com>
2023-03-19 00:54:15 +0000

    Rework home page

--- lib/svoenix_web/live/home_live.ex
+++ lib/svoenix_web/live/home_live.ex
@@ -6,7 +6,7 @@ defmodule SvoenixWeb.HomeLive do
 
   def render(assigns) do
     ~H"""
-    <section class="container flex flex-col items-center gap-12 px-4 py-12">
+    <section class="container flex flex-col items-center gap-28 px-4 py-12">
       <header class="flex flex-col items-center gap-1">
         <p class="text-4xl">👨‍💻☕️</p>
         <h1 class="tracking widest text-4xl font-bold">IRL café</h1>
@@ -15,17 +15,18 @@ defmodule SvoenixWeb.HomeLive do
         </h2>
       </header>
 
-      <div class="flex items-center space-x-4">
+      <div class="flex flex-wrap justify-center items-center gap-6">
         <.form id="city-form" for={@form} class="relative" phx-submit="save">
           <input
             id="city-name-input"
             name="name"
             type="text"
-            placeholder="lisbon"
+            placeholder={Enum.random(["🇵🇹 lisbon", "🇫🇷 paris", "🇬🇧 london"])}
             class={[
-              "w-[12ch] py-4 px-5 text-5xl placeholder:text-gray-400 hover:bg-gray-100 rounded-md",
-              "border-none ring-none outline-none",
-              "focus:border-transparent focus:ring-transparent focus:shadow-none"
+              "w-full sm:w-[12ch] py-4 px-5 text-4xl placeholder:text-3xl placeholder:text-opacity-50 placeholder:text-gray-400 bg-gray-100/75 shadow-inner hover:bg-gray-100 rounded-md",
+              "border border-gray-50 ring-transparent outline-transparent",
+              "focus:border-gray-200 focus:bg-gray-100 focus:ring-transparent",
+              "transition duration-75 text-center"
             ]}
             autocomplete="off"
             required
@@ -33,7 +34,7 @@ defmodule SvoenixWeb.HomeLive do
         </.form>
 
         <button
-          class="flex items-baseline bg-brand text-white px-4 py-2.5 rounded shadow-lg group"
+          class="w-full sm:w-[12ch] flex justify-center items-baseline bg-brand text-white px-6 py-3.5 rounded group"
           phx-disable-with="exploring..."
         >
           <span class="text-lg font-semibold">explore</span>

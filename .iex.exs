494cd0822785808d80668e88097b03534e4e71f6
Basile Nouvellet <b.nouvellet@gmail.com>
2023-03-19 22:36:58 +0000

    Add .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,9 @@
+import_file_if_available("~/.iex.exs")
+
+alias Svoenix.Repo
+alias Svoenix.Bookings
+alias Svoenix.Bookings.Booking
+alias Svoenix.Places
+alias Svoenix.Places.Place
+alias Svoenix.Cities
+alias Svoenix.Cities.City
